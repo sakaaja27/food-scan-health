34ddc31a67680b39d027f36c36fd25d2866d14ce
Bramasta <bramastasaka9@gmail.com>
2025-06-20 23:22:23 +0700

    add DOM image input change value

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import { BsCamera } from "react-icons/bs";
 export default function App() {
   return (
@@ -11,23 +12,59 @@ export default function App() {
 }
 
 function ContentImage() {
+  const [image, setImage] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
   return (
     <div className="relative w-60 h-60 mx-auto">
-      <Image></Image>
-      <button className="absolute bottom-1 right-5 bg-white p-3 rounded-full shadow-lg">
+      <Image
+        image={image}
+        fileInputRef={fileInputRef}
+        onImageUpload={handleImageUpload}
+      ></Image>
+      <button
+        onClick={handleButtonClick}
+        className="absolute bottom-1 right-5 bg-white p-3 rounded-full shadow-lg"
+      >
         <BsCamera className="text-blue-500 w-7 h-7" />
       </button>
     </div>
   );
 }
 
-function Image() {
+function Image({ image, onImageUpload, fileInputRef }) {
   return (
     <div className="rounded-full overflow-hidden w-full h-full">
-      <img
-        src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"
-        alt="Beautiful Landscape"
-        className="w-full h-auto objcet-cover shadow-md"
+      {image ? (
+        <img
+          src={image}
+          alt="Image Preview"
+          className="w-full h-auto object-cover shadow-md"
+        />
+      ) : (
+        <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+          <span className="text-gray-400">No Image</span>
+        </div>
+      )}
+      <input
+        type="file"
+        accept="image/*"
+        className="hidden"
+        ref={fileInputRef}
+        onChange={onImageUpload}
       />
     </div>
   );
